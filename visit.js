bda7f28f15f78e06e696290d2212277b464d3b6d
kunnan <hongkunnan1997@gmail.com>
2021-05-09 09:08:37 +0800

    no message

--- visit.js
+++ visit.js
@@ -2,9 +2,6 @@ function visit() {
     var db = firebase.database();
     var date = new Date();
     var today = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-    $.get("http://ip-api.com/json", function(response) {
-      console.log(response.country);
-    }, "jsonp");
     var ref = db.ref(today).child("visits");
 
     ref.transaction(current_value => {
