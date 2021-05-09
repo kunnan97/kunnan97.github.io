e3f6c7f98ae54472ce417cf335cf96592070fb4b
kunnan <hongkunnan1997@gmail.com>
2021-05-09 09:39:03 +0800

    no message

--- visit.js
+++ visit.js
@@ -1,13 +1,14 @@
 function visit() {
-    var db = firebase.database();
-    var date = new Date();
-    var today = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-    var ref = db.ref(today).child("visits");
     $.get("https://ipinfo.io", function(response) {
-        console.log(response.country);
-    }, "jsonp");
-    ref.transaction(current_value => {
+      var db = firebase.database();
+      var date = new Date();
+      var today = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+      var country = response.country;
+      var ref = db.ref(today).child(country);
+
+      ref.transaction(current_value => {
         return (current_value || 0) + 1;
-    });
+      });
+    }, "jsonp");
   }
 window.onload = visit;
